6c53747cc8db2aaf7d6729b29fe616c739d57c83
Carlos Gumucio <carlosignacio.gumucio@externos-cl.cencosud.com>
2025-09-26 19:09:52 -0300

    fix: schema nullable fields

---
+++ functions/migrations/20250926220628_make_updated_at_nullable/migration.sql
@@ -0,0 +1,74 @@
+-- AlterTable
+ALTER TABLE "areas_servicio" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "carrito" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "categorias" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "clientes" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "comentarios" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "compras_colectivas" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "comunas" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "descuentos_cantidad" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "direcciones" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "escalas_precios" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "items_pedido" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "lista_deseos" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "notificaciones" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "pagos" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "participantes_colectivos" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "pedidos" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "planes" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "productos" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "progreso_campanas" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "proveedores" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "publicidad" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "regiones" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "solicitudescontacto" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "suscripciones" ALTER COLUMN "updated_at" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "usuarios" ALTER COLUMN "updated_at" DROP NOT NULL;
